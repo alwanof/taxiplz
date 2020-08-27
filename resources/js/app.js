2ef2bcbe0b0b5d7f1bb7202fade7aa4227e2364d
alwanof <free1soft@gmail.com>
2020-08-27 17:49:47 +0300

    fix api

--- resources/js/app.js
+++ resources/js/app.js
@@ -73,12 +73,12 @@ if (location.protocol !== 'https:') {
 // cd /home/marasiel.com/public_html/taxiplzz
 const CONFIG = {
 
-    API_URL: http + window.location.hostname + '/taxi/public/api/',
+    /*API_URL: http + window.location.hostname + '/taxi/public/api/',
     PATH: '/taxi/public',
-    FULL_PATH: 'http://' + window.location.hostname + '/taxi/public',
-    /*API_URL: http + window.location.hostname + '/taxiplzz/public/api/',
+    FULL_PATH: 'http://' + window.location.hostname + '/taxi/public',*/
+    API_URL: http + window.location.hostname + '/taxiplzz/public/api/',
     PATH: '/taxiplzz/public',
-    FULL_PATH: http + window.location.hostname + '/taxiplzz/public',*/
+    FULL_PATH: http + window.location.hostname + '/taxiplzz/public',
     LANG: translations,
     PARSE: Parse,
     LIVEQ: client,
